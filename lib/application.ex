86bb6cbea61719d9f574e79dcd4ddecd5294add6
Laura-Sirianni <laura.melo@petlove.com.br>
2020-03-14 20:27:22 -0300

    Feat: Add Plug and Cowboy
    Add Plug and Cowboy to receive HTTP requests and configure Application
    to start web server

--- lib/application.ex
+++ lib/application.ex
@@ -4,7 +4,8 @@ defmodule Rover.Application do
   def start(_type, _args) do
     children = [
       Supervisor.child_spec({Registry, [keys: :unique, name: Rover.Registry]}, id: :rover_registry),
-      Supervisor.child_spec({RoverSupervisor, []}, id: RoverSupervisor)
+      Supervisor.child_spec({RoverSupervisor, []}, id: RoverSupervisor),
+      Plug.Adapters.Cowboy.child_spec(:http, Rover.Web.Router, [], port: 3000)
     ]
 
     opts = [strategy: :one_for_one, name: Application.Supervisor]
